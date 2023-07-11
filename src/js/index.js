04be171d261ab1f9c00b4da3bbd1925c1a3bfeaf
Artyom462 <131834496+Artyom462@users.noreply.github.com>
2023-07-11 00:26:47 +0000

    s

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,9 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Home from "./Home.js";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
